5379c7817492acf3233247416fcb5b5eb93dae50
P. Vibet <pvibet@gmail.com>
2018-01-05 07:43:54 +0100

    * Add function to generate PROTECTED_DOWNLOADS array

--- variables.php
+++ variables.php
@@ -7,8 +7,30 @@
  *	Sets the admin password to generate a new download link
  *	Sets a date when the file will expire (examples: +1 year, +5 days, +13 hours)
  */	
+
+        // functions
+        function dirToArray($dir) {
+          $result = array();
+          $cdir = scandir($dir);
+          foreach ($cdir as $key => $value)
+          { 
+            if($value === '.' || $value === '..') {continue;}
+            if(is_file("$dir/$value")) {
+              //$result[]="$dir/$value";
+              $result[$value]['content_type'] = mime_content_type("$dir/$value");
+              $result[$value]['suggested_name'] = $value;
+              $result[$value]['protected_path'] = "$dir/$value";
+              continue;
+            }
+            foreach(dirToArray("$dir/$value") as $value)
+            { 
+              $result[]=$value;
+            }
+           }
+           return $result;
+        }
  	
-	// Arrays of content type, suggested names and protected names
+	/* Arrays of content type, suggested names and protected names
 	$PROTECTED_DOWNLOADS = array(
 		array(
 			'content_type' => 'application/zip', 
@@ -20,7 +42,10 @@
 			'suggested_name' => 'star.zip', 
 			'protected_path' => 'secret/file2.zip'
 		)
-	);
+	); */
+
+        $directory = '/download_folder';
+        $PROTECTED_DOWNLOADS = dirToArray($directory);
 
 	// The path to the download.php file (probably same dir as this file)
  	define('DOWNLOAD_PATH','/singleuse/download.php');
